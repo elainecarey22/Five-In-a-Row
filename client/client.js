4734fc8e6cb2c57d7941a176aa5e144663df31fd
Elaine <46967987+elainecarey22@users.noreply.github.com>
2021-02-07 20:44:32 +0000

    Add two player functionality to update table

--- client/client.js
+++ client/client.js
@@ -3,6 +3,8 @@ const readline = require('readline');
 
 let gameState;
 let move;
+let playerId;
+let baseUrl = 'http://localhost:3080/';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -16,19 +18,19 @@ const startGame = () => {
             var player = { user: name };
             request.post({
                 headers: { 'content-type': 'application/json' },
-                url: 'http://localhost:3080/api/user',
+                url: `${baseUrl}api/user`,
                 body: JSON.stringify(player)
             },
                 function (error, response, body) {
-                    gameState = body;
+                    playerId = JSON.parse(body).id;
                     // Continuous request to check state of game
                     const requestLoop = setInterval(function () {
                         request.get({
-                            url: 'http://localhost:3080/api/game'
+                            url: `${baseUrl}api/game`
                         },
                             function (error, response, body) {
                                 gameState = JSON.parse(body);
-                                if (gameState.ready === false) {
+                                if (gameState.ready === false) {              
                                     console.log('Waiting on another player to join...');
                                 } else {
                                     // TODO: fix logic to stop the below message from displaying when more than 2 players have joined
@@ -39,56 +41,83 @@ const startGame = () => {
                                     }
                                 }
                             });
-                    }, 2000);
+                    }, 5000);
                 });
             // rl.close();
         });
     })
 }
 
-const showBoard = () => {
-    return new Promise((resolve, reject) => {
+const game = () => {
+    const requestLoop = setInterval(function () {
         request.get({
-            url: 'http://localhost:3080/api/board'
+            url: `${baseUrl}api/game`
         },
-        function(error, response, body) {
-            console.log(body);
-        })
-    resolve()
-    })
+            function (error, response, body) {
+                gameState = JSON.parse(body);
+                if (gameState.playerTurn == playerId) {
+                    console.log('Your turn');
+                    printTable(gameState.table);
+                    playerMove();
+                    clearInterval(requestLoop);
+                } else {
+                    console.log('Other players turn');
+                }
+
+            });
+    }, 5000);
 }
 
 const playerMove = () => {
-    return new Promise((resolve, reject) => {
-        rl.question('Enter a column 1-9 \n', (column) => {
-            move = {user: column}
-            request.post({
-                headers: { 'content-type': 'application/json' },
-                url: 'http://localhost:3080/api/board',
-                body: JSON.stringify(move)
-            },
+    rl.question('Enter a column 1-9 \n', (column) => {
+        move = { playerId, column }
+        //TODO: make sure input is only 1-9
+        request.post({
+            headers: { 'content-type': 'application/json' },
+            url: `${baseUrl}api/board`,
+            body: JSON.stringify(move)
+        },
             function (error, response, body) {
-                newMove = JSON.parse(body);
-                // prints all of tableData as it is in server.js
-                showBoard();
+                let gameState = JSON.parse(body);
+                printTable(gameState.table);
+                game();
             })
-        })
     });
+}
+
+const printTable = (table) => {
+    const rowCount = 6;
+    const columnCount = table.length;
+
+    for (let r = 0; r < rowCount; r++) {
+        let rowStr = '';
+
+        for (let c = 0; c < columnCount; c++) {
+            const tableColPos = rowCount - (r + 1);
+
+            const val = table[c][tableColPos]
+
+            if (val) {
+                rowStr += '[' + val + ']';
+            } else {
+                rowStr += '[ ]';
+            }
+        }
+        console.log(rowStr)
+    }
 }
 
 const main = async () => {
     await startGame()
-    await showBoard()
-    await playerMove()
-    // await makeMove()
-    rl.close()
+    game();
+    // rl.close()
 }
 
 main()
 
-    // TODO: use below SIGINT event to listen for player who wants to exit
-    // rl.on('SIGINT', () => {
-    //     rl.question('Are you sure you want to exit? ', (answer) => {
-    //       if (answer.match(/^y(es)?$/i)) rl.pause();
-    //     });
-    //   });
+// TODO: use below SIGINT event to listen for player who wants to exit
+// rl.on('SIGINT', () => {
+//     rl.question('Are you sure you want to exit? ', (answer) => {
+//         if (answer.match(/^y(es)?$/i)) rl.pause();
+//     });
+// });
--- server/server.js
+++ server/server.js
@@ -2,77 +2,47 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const table = require('table');
 
 app.use(cors());
 app.use(bodyParser.json());
 
 port = 3080;
-gameState = { ready: false, users: [] };
+const gameTable = [[],[],[], [], [], [], [], [], []];
+gameState = { ready: false, users: [], playerTurn: null, table: gameTable };
 
-tableData = [
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""]
-]
 
-// Creating column width configuration 
-config = {
-    columns: {
-        0: {
-            width: 1   // Columns of width 1 
-        },
-        1: {
-            width: 1
-        },
-        2: {
-            width: 1
-        },
-        3: {
-            width: 1
-        },
-        4: {
-            width: 1
-        },
-        5: {
-            width: 1
-        },
-        6: {
-            width: 1
-        },
-        7: {
-            width: 1
-        },
-        8: {
-            width: 1
-        }
-    }
-};
-let board = table.table(tableData, config);
+function gameMove(column, value) {
+    gameState.table[column].push(value);
+}
 
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
 app.post('/api/user', (req, res) => {
-    // console.log(req.body.user);
+
     const user = req.body.user;
-    if (gameState.users.length === 2) {
-        res.send('Game is full');
-    } else {
-        gameState.users.push(user);
-        if (gameState.users.length === 1) {
-            res.send(gameState);
+    //TODO: handle this differently
+    // if (gameState.users.length === 2) {
+    //     res.send('Game is full');
+    // } else {
+
+        if (gameState.users.length === 0) {
+            p1 = { user, id: 1 }
+            gameState.users.push(p1);
+            res.send(p1);
         }
-        if (gameState.users.length === 2) {
+        else if (gameState.users.length === 1) {
+            p2 = { user, id: 2 };
+            gameState.users.push(p2);
+            gameState.playerTurn = 1;
             gameState.ready = true;
-            res.send(gameState);
+            res.send(p2);
         }
-    }
-    console.log(gameState.users);
+        else {
+            res.send("Game is full");
+        }
+  //  }
 });
 
 app.get('/api/game', (req, res) => {
@@ -80,18 +50,21 @@ app.get('/api/game', (req, res) => {
 });
 
 app.get('/api/board', (req, res) => {
-    // change this back to BOARD when figured out
-    res.send(tableData);
+    res.send(gameState);
 });
 
-// takes user input and puts it in 6th position of first array
-// TODO: have user input go to column (array) specified
 app.post('/api/board', (req, res) => {
-    const move = req.body.user;
-    console.log(move);
-    tableData[0].splice(6, 1, move);
-    console.log(tableData);
-    res.send(tableData);
+    const playerMove = req.body;
+
+    if (playerMove.playerId == 1) {
+        gameMove(playerMove.column - 1, "X");
+        gameState.playerTurn = 2;
+    } else {
+        gameMove(playerMove.column - 1, "O");
+        gameState.playerTurn = 1;
+    }
+
+    res.send(gameState);
 });
 
 app.listen(port, () => {
